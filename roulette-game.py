38973a3bcfbea3920088a062c7efcf94834e64a4
Arippz <ahmadarifinmaulidan123@gmail.com>
2026-01-14 19:45:33 +0700

    Add player class but not done yet

--- roulette-game.py
+++ roulette-game.py
@@ -1,18 +1,34 @@
 from PySide6.QtCore import QSize, Qt
-from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
+from PySide6.QtGui import QAction
+from PySide6.QtWidgets import (
+    QApplication,
+    QMainWindow,
+    QPushButton,
+    QLabel,
+    QLineEdit,
+    QVBoxLayout,
+    QWidget,
+    QMenu,
+)
 
 import sys
 
+if rock == True and scissor == True:
+    scissor == False
+
+class Player():
+    def __init__(self, lives):
+        self.lives = lives
+
+    def doAction(self):
+        
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super().__init__()
-        self.setWindowTitle("My App")
-        
-        button = QPushButton("Press Me")
 
-        self.setCentralWidget(button)
 
+    
 app = QApplication(sys.argv)
 
 window = MainWindow()
